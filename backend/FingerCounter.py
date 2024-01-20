ad316e6ddf2b8a4dedd9a0d25cb6ec79d54be108
Jason Baek <bkjwjason@gmail.com>
2024-01-20 21:46:16 +0800

    reset fingerCounter

--- backend/FingerCounter.py
+++ backend/FingerCounter.py
@@ -1,48 +1,55 @@
 import cv2
+import time
 import os
 import HandTrackingModule as htm
 
-img = cv2.imread('0Left.jpg')
+wCam, hCam = 640, 480
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+
+pTime = 0
 
 detector = htm.handDetector(detectionCon=0.75)
 
 tipIds = [4, 8, 12, 16, 20]
 
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+    # print(lmList)
+    handType = detector.findHandType(img)
 
-img = detector.findHands(img)
-lmList = detector.findPosition(img, draw=False)
-#print(lmList)
+    if len(lmList) != 0:
+        fingers = []
 
+        if handType == 'Right':
+            if lmList[tipIds[0]][1] < lmList[tipIds[0] - 1][1]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        
+        elif handType == 'Left':
+            if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][1]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
 
+        # 4 Fingers
+        for id in range(1, 5):
+            if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
 
-handType = detector.findHandType(img)
+        # print(fingers)
+        totalFingers = fingers.count(1)
+        print(totalFingers)
 
-if len(lmList) != 0 and handType is not None:
-    fingers = []
-    # Thumb
-    print(handType)
-    if handType == 'Right':
-        if lmList[tipIds[0]][1] < lmList[tipIds[0] - 1][1]:
-            fingers.append(1)
-        else:
-            fingers.append(0)
-    
-    elif handType == 'Left':
-        if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][1]:
-            fingers.append(1)
-        else:
-            fingers.append(0)
-
-    # 4 Fingers
-    for id in range(1, 5):
-        if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
-            fingers.append(1)
-        else:
-            fingers.append(0)
-
-    #print(fingers)
-    totalFingers = fingers.count(1)
-    print(totalFingers)
-
-cv2.imshow("Image", img)
-cv2.waitKey(0)
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
