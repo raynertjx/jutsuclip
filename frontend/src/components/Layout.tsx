d981120480686d397dbff849058554774e7fb889
raynertjx <rayner742@gmail.com>
2024-01-20 19:07:36 +0800

    add basic fields

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,12 +1,18 @@
 import React, { FC } from "react";
+import { FieldForm } from "./form/Form";
 
 interface LayoutProps {}
 
 const Layout: FC<LayoutProps> = () => {
   return (
-    <div>
-      <h1 className="text-2xl">JutsuClip</h1>
-      <p>Unleash the power of seamless productivity with JutsuClip!</p>
+    <div className="flex justify-center">
+      <div className="w-3/5">
+        <div className="pb-4">
+          <h1 className="text-2xl">JutsuClip</h1>
+          <p>Unleash the power of seamless productivity with JutsuClip!</p>
+        </div>
+        <FieldForm />
+      </div>
     </div>
   );
 };
